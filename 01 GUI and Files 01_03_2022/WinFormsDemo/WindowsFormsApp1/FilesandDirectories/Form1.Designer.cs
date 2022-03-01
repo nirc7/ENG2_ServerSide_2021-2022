e5d5a9b9dceecbbd6d0322011df8a09ff2e08f65
LabM RUPPIN <labm@ruppin.ac.il>
2022-03-01 12:12:38 +0200

    file2

---
+++ 01 GUI and Files 01_03_2022/WinFormsDemo/WindowsFormsApp1/FilesandDirectories/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace FilesandDirectories
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWirte = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReadLines = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWirte
+            // 
+            this.btnWirte.Location = new System.Drawing.Point(40, 53);
+            this.btnWirte.Name = "btnWirte";
+            this.btnWirte.Size = new System.Drawing.Size(75, 23);
+            this.btnWirte.TabIndex = 0;
+            this.btnWirte.Text = "Write2File";
+            this.btnWirte.UseVisualStyleBackColor = true;
+            this.btnWirte.Click += new System.EventHandler(this.btnWirte_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(40, 141);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(96, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "ReadFromFile";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnReadLines
+            // 
+            this.btnReadLines.Location = new System.Drawing.Point(173, 141);
+            this.btnReadLines.Name = "btnReadLines";
+            this.btnReadLines.Size = new System.Drawing.Size(119, 23);
+            this.btnReadLines.TabIndex = 3;
+            this.btnReadLines.Text = "ReadFromFileLine";
+            this.btnReadLines.UseVisualStyleBackColor = true;
+            this.btnReadLines.Click += new System.EventHandler(this.btnReadLines_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(154, 53);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(113, 23);
+            this.btnOpenFile.TabIndex = 4;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnReadLines);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWirte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWirte;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReadLines;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFile;
+    }
+}
+
---
+++ 01 GUI and Files 01_03_2022/WinFormsDemo/WindowsFormsApp1/FilesandDirectories/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilesandDirectories
+{
+    public partial class Form1 : Form
+    {
+        string path = "../../MyFiles/MyData.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWirte_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText(path, "hello dudi " + new Random().Next(100) + "\n");
+            GetAllData();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            label1.Text = File.ReadAllText(path);
+        }
+
+        private void btnReadLines_Click(object sender, EventArgs e)
+        {
+            GetAllData();
+        }
+
+        private void GetAllData()
+        {
+            int count = 1;
+            label1.Text = "";
+            string[] lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                label1.Text += count++ + " -- " + line + "\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllData();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            path = openFileDialog1.FileName;
+            GetAllData();
+        }
+    }
+}
